2e90fefb90f90c1d6f8ffb2be48a9a42aa2bf19d
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2018-10-27 01:26:24 -0300

    change to stateless component

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 import { fetchCategories } from '../../actions/categoriesActions'
 import {
@@ -11,47 +11,40 @@ import {
 
 import DoneIcon from '@material-ui/icons/Done'
 import { Link } from 'react-router-dom'
-
-
 import './categories.css'
 
-
-
-class Categories extends Component {
-
-    render() {
-        const { categories } = this.props
-        const selectedCategory = this.props.category
-        return (
-            <Grid item xs={12}>
-                <Card className="spacing-top">
-                    <CardContent>
-                        <Typography variant="subheading" color="textSecondary">
-                            Categories:
+function Categories(props) {
+    const { categories } = props
+    const selectedCategory = props.category
+    return (
+        <Grid item xs={12}>
+            <Card className="spacing-top">
+                <CardContent>
+                    <Typography variant="subheading" color="textSecondary">
+                        Categories:
                         </Typography>
-                        <div className="categories">
-                            {categories && (
-                                <Link to="/" className="category-link">
-                                    {selectedCategory === undefined ?
-                                        <Chip label="All" className="category" clickable color="primary" icon={<DoneIcon />} />
-                                        : <Chip label="All" className="category" clickable />
-                                    }
-                                </Link>
-                            )}
-                            {categories && categories.items.map(category => (
-                                <Link to={`/${category.name}`} key={category.name} className="category-link">
-                                    {selectedCategory !== undefined && category.name === selectedCategory ?
-                                        <Chip label={category.name} className="category" key={category.name} clickable color="primary" icon={<DoneIcon />} />
-                                        : <Chip label={category.name} className="category" key={category.name} clickable />
-                                    }
-                                </Link>
-                            ))}
-                        </div>
-                    </CardContent>
-                </Card>
-            </Grid >
-        )
-    }
+                    <div className="categories">
+                        {categories && (
+                            <Link to="/" className="category-link">
+                                {selectedCategory === undefined ?
+                                    <Chip label="All" className="category" clickable color="primary" icon={<DoneIcon />} />
+                                    : <Chip label="All" className="category" clickable />
+                                }
+                            </Link>
+                        )}
+                        {categories && categories.items.map(category => (
+                            <Link to={`/${category.name}`} key={category.name} className="category-link">
+                                {selectedCategory !== undefined && category.name === selectedCategory ?
+                                    <Chip label={category.name} className="category" key={category.name} clickable color="primary" icon={<DoneIcon />} />
+                                    : <Chip label={category.name} className="category" key={category.name} clickable />
+                                }
+                            </Link>
+                        ))}
+                    </div>
+                </CardContent>
+            </Card>
+        </Grid >
+    )
 }
 
 const mapStateToProps = ({ categories }) => {
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import { Grid, Card, CardContent, Typography } from '@material-ui/core';
 
-class NotFound extends Component {
 
-    render() {
-        return (
-            <Grid container spacing={8} justify="center">
-                <Card className="card notfound">
-                    <CardContent>
-                        <Typography variant="subheading" component="h2">404: PAGE NOT FOUND</Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-        )
-    }
+function NotFound() {
+    return (
+        <Grid container spacing={8} justify="center">
+            <Card className="card notfound">
+                <CardContent>
+                    <Typography variant="subheading" component="h2">404: PAGE NOT FOUND</Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
 }
 
 export default NotFound
